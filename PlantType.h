12aa0f3232ff70b0695f21d6b27cd87b54f9a08e
TheDzashe <dzashemkwarbz@gmail.com>
2025-10-21 22:18:18 +0200

    update PlantType.h
    created class with Doxygen

--- PlantType.h
+++ PlantType.h
@@ -1,3 +1,21 @@
-/*
-Base component class for all plants. Leaf-level plants derive from this (e.g., Rose, WaterLily).
+/**
+ * @file PlantType.h
+ * @brief Base component class for the type of plants hierarchy. This class participates in multiple design patterns.
+ *
 */
+
+#ifndef PLANTTYPE_H
+#define PLANTTYPE_H
+
+
+/**
+ * @brief Participates in the following design patterns:
+ * 1. Bridge - abstraction for the bridge design pattern
+ * 
+ */
+
+class PlantType{
+
+};
+
+#endif
