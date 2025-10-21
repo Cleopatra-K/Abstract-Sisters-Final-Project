3ecaf536383b004e0d85b4ddd85331557cb7c001
cleoluvs2code <133550525+Cleopatra-K@users.noreply.github.com>
2025-10-21 22:49:48 +0200

    i have made the header files for the prodouct of the factory dp.

--- Indoor.h
+++ Indoor.h
@@ -1,3 +1,23 @@
 /*
 Possible subtypes (categories) of plant factories.
 */
+#ifndef INDOOR_H
+#define INDOOR_H
+#include "PlantType.h"
+
+/**
+ * @class IndoorPlant
+ * @brief Plants that live inside your house
+ * @author Cleopatra
+ * 
+ * These plants like being inside where it's warm and safe.
+ * Examples: Peace Lilies, Spider Plants, Succulents
+ */
+class IndoorPlant : public PlantType {
+public:
+    IndoorPlant(const std::string& n, double p, const std::string& desc);
+    virtual PlantType* clone() const override = 0;
+    std::string getCategory() const override;
+    void display() const override;
+};
+#endif
