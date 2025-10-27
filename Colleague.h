d70ca852df3a7c4754a631c421bdd4f3e7b81181
Kgaugelo Matsena <kcnode1@gmail.com>
2025-10-27 20:46:58 +0200

    Colleague

---
+++ Colleague.h
@@ -0,0 +1,24 @@
+/*
+Abstract base class for components (Staff, Customer) that communicate through the Mediator.
+*/
+
+#ifndef COLLEAGUE_H
+#define COLLEAGUE_H
+
+#include <string>
+#include "Mediator.h"
+
+class Colleague {
+protected:
+    Mediator* mediator;
+    std::string name;
+public:
+    Colleague(Mediator* med, const std::string& n): mediator(med), name(n) {}
+    virtual ~Colleague();
+
+    virtual void receiveMessage(const std::string& message) = 0;
+    virtual void sendMessage(const std::string& message);
+    virtual std::string getName(){ return name;};
+};
+
+#endif
