8546f89cf10f94aa8ebe92406a4fa2a51626f762
TheDzashe <dzashemkwarbz@gmail.com>
2025-10-28 14:14:52 +0200

    updated the composite pattern

---
+++ Indoor.cpp
@@ -0,0 +1,5 @@
+#include "Indoor.h"
+
+Indoor::Indoor(const std::string& n, double p, const std::string& desc, std::string &colourType, std::string season):PlantType(n,p,desc, colourType, season){
+
+}
