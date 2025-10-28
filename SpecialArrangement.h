adfa72f60e9ce9f876cb9bbbb7478076412d6750
Jessica Marodi <u20445564@tuks.co.za>
2025-10-28 19:31:50 +0200

    Add files via upload

--- SpecialArrangement.h
+++ SpecialArrangement.h
@@ -1,12 +1,26 @@
 #ifndef SPECIALARRANGEMENT_H
 #define SPECIALARRANGEMENT_H
 #include "PlantDecorator.h"
+
 /*
-Special arrangement.
+Adds a decorative pot to a plant.
 */
 class SpecialArrangement : public PlantDecorator{
- 
+ /**
+     * @brief Performs the gift wrap operation
+     * Adds pot decorator functionality to the base plant operation
+     */
     void decorate() override;
+        /**
+     * @brief Destructor
+     */
+    ~SpecialArrangement() override{}
+     /**
+     * @brief Constructor
+     * @param plant The PlantType component to be arranged specially
+     */
+    SpecialArrangement(PlantType* plant);
+    std::string message;
 };
 
 #endif //SPECIALARRANGEMENT_H
