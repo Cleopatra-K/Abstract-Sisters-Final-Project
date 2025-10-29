041cb4745423ca05c4309e9f39af892e953f717f
TheDzashe <dzashemkwarbz@gmail.com>
2025-10-29 12:47:57 +0200

    fixed missing brackets
    fixed the missing brackets in Mangrove.cpp, BonsaiTree.cpp and WaterLily.cpp

--- BonsaiTree.cpp
+++ BonsaiTree.cpp
@@ -15,6 +15,7 @@ void BonsaiTree::remove(PlantType* plant){
 std::vector<PlantType*> BonsaiTree::getChildren(){
     return std::vector<PlantType*>();
 }
+
 BonsaiTree::BonsaiTree() : Indoor("Bonsai Tree", 60.0, "Miniature cultivated tree") {}
 
 BonsaiTree::BonsaiTree(const std::string& n, double p, const std::string& desc, std::string& colourType, std::string season) 
--- Mangrove.cpp
+++ Mangrove.cpp
@@ -14,6 +14,8 @@ void Mangrove::remove(PlantType* plant){
 
 std::vector<PlantType*> Mangrove::getChildren(){
     return std::vector<PlantType*>();
+}
+
 Mangrove::Mangrove() : Aquatic("Mangrove", 55.0, "Coastal tree that grows in saltwater") {}
 
 Mangrove::Mangrove(const std::string& n, double p, const std::string& desc, std::string& colourType, std::string season) 
