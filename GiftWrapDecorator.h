d47347ad3f2aa76585979e4f42cb7d005a06b3e0
Jessica Marodi <u20445564@tuks.co.za>
2025-10-30 18:11:44 +0200

    Refactor GiftWrapDecorator for consistency

--- GiftWrapDecorator.h
+++ GiftWrapDecorator.h
@@ -9,8 +9,8 @@ public:
     GiftWrapDecorator(PlantType* plant) : PlantDecorator(plant) {}
 
     void decorate() override {
-        if (decoratedPlant) {
-            decoratedPlant->decorate();
+        if (this->decoratedPlant) {
+            this->decoratedPlant->decorate();
             std::cout << "\033[35m"
                   << " + Wrapped beautifully for gifting!" << "\033[0m"<< std::endl;
         }
@@ -18,17 +18,18 @@ public:
 
     double orginalPrice = this->getPrice();
     double getPrice() override {
-        return decoratedPlant ? decoratedPlant->getPrice() + 10.0 : orginalPrice;
+        return this->decoratedPlant ? this->decoratedPlant->getPrice() + 10.0 : orginalPrice;
     }
 
     std::string getDescription() override {
-        return decoratedPlant ? decoratedPlant->getDescription() + " with Gift Wrap" : " No Gift Wrap";
+        return this->decoratedPlant ? this->decoratedPlant->getDescription() + " with Gift Wrap" : " No Gift Wrap";
     }
     GiftWrapDecorator::~GiftWrapDecorator() {
     }
 };
 
 #endif
+
 
 
 
